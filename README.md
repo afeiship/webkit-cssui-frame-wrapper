5588847d99e8b2904cc5954e4f353263414eb183
afeiship <1290657123@qq.com>
2019-07-24 09:49:08 +0800

    refactor: use gulp4

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # webkit-sassui-frame-wrapper
 > Webkit sass ui module media wrapper.
 
-## usage:
+## usage
 ```html
 <div class="frame-wrapper av1">
   <img src="https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png" alt="">
 </div>
 ```
 
-## resources:
-+ https://github.com/afeiship/generator-webkit-cssui
+## resources
 + http://www.zhangxinxu.com/study/201503/css3-object-fit.html
--- build/styles.js
+++ build/styles.js
@@ -1,26 +1,21 @@
 (function() {
+  "use strict";
 
-  'use strict';
-
-  var gulp = require('gulp');
-  var config = require('./config');
-  var argv = require('yargs').argv;
-  var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'del']
+  var gulp = require("gulp");
+  var config = require("./config");
+  var argv = require("yargs").argv;
+  var $ = require("gulp-load-plugins")({
+    pattern: ["gulp-*", "gulp.*", "del"]
   });
 
-
-  gulp.task('styles',function() {
-    return gulp.src('src/*.scss')
-      .pipe(gulp.dest('dist'))
-      .pipe($.sass({outputStyle: 'expanded'}).on('error', $.sass.logError))
-      .pipe(gulp.dest('dist'))
-      .pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
-      .pipe($.rename({
-        extname:'.min.css'
-      }))
-      .pipe(gulp.dest('dist'));
+  gulp.task("styles", function() {
+    return gulp
+      .src("src/*.scss")
+      .pipe(gulp.dest("dist"))
+      .pipe($.sass({ outputStyle: "expanded" }).on("error", $.sass.logError))
+      .pipe(gulp.dest("dist"))
+      .pipe($.sass({ outputStyle: "compressed" }).on("error", $.sass.logError))
+      .pipe($.rename({ extname: ".min.css" }))
+      .pipe(gulp.dest("dist"));
   });
-
-
-}());
+})();
