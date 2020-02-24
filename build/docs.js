251f78e50ec66221230ed2ec3851276c78496e15
afeiship <1290657123@qq.com>
2020-02-24 14:05:47 +0800

    fix: update include paths

--- build/docs.js
+++ build/docs.js
@@ -2,6 +2,8 @@
   'use strict';
 
   var gulp = require('gulp');
+  var path = require('path');
+  var SASS_INCLUDE_PATHS = [path.join(__dirname, '..', 'node_modules')];
   var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del'],
   });
@@ -10,7 +12,12 @@
   gulp.task('docs', function() {
     return gulp
       .src('docs/style.scss')
-      .pipe($.sass({ outputStyle: 'expanded' }).on('error', $.sass.logError))
+      .pipe(
+        $.sass({
+          outputStyle: 'expanded',
+          includePaths: SASS_INCLUDE_PATHS,
+        }).on('error', $.sass.logError)
+      )
       .pipe(gulp.dest('docs'));
   });
 })();
--- build/styles.js
+++ build/styles.js
@@ -2,7 +2,9 @@
   'use strict';
 
   var gulp = require('gulp');
+  var path = require('path');
   var autoprefixer = require('autoprefixer');
+  var SASS_INCLUDE_PATHS = [path.join(__dirname, '..', 'node_modules')];
   var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del', '@feizheng/gulp-*'],
   });
@@ -13,7 +15,7 @@
       .src('src/*.scss')
       .pipe($.feizheng.pkgHeader())
       .pipe(gulp.dest('dist'))
-      .pipe($.sass())
+      .pipe($.sass({ includePaths: SASS_INCLUDE_PATHS }))
       .pipe($.postcss([autoprefixer()]))
       .pipe(gulp.dest('dist'));
   });
